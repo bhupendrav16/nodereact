f4a1699f855e673f0701616bd03fa99965e4619b
bhupendrav16 <bhupendra.v.brudite@gmail.com>
2024-07-10 11:41:15 +0530

    added files

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return(
+        <div className="header">
+            <h1>User creation</h1>
+            <p>Created by</p>
+            <p>ere ere</p>
+        </div>
+    )
+}
